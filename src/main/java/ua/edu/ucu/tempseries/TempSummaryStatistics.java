b8334e45c3328780ac5fc63b6c0c05da6b400ced
Max <hrytsay@ucu.edu.ua>
2019-10-16 15:44:09 +0300

    Design fixed

--- src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
+++ src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
@@ -1,10 +1,42 @@
 package ua.edu.ucu.tempseries;
 
 public class TempSummaryStatistics {
-    double avgTemp;
-    double devTemp;
-    double minTemp;
-    double maxTemp;
+    public double getAvgTemp() {
+        return avgTemp;
+    }
+
+    public void setAvgTemp(double avgTemp) {
+        this.avgTemp = avgTemp;
+    }
+
+    public double getDevTemp() {
+        return devTemp;
+    }
+
+    public void setDevTemp(double devTemp) {
+        this.devTemp = devTemp;
+    }
+
+    public double getMinTemp() {
+        return minTemp;
+    }
+
+    public void setMinTemp(double minTemp) {
+        this.minTemp = minTemp;
+    }
+
+    public double getMaxTemp() {
+        return maxTemp;
+    }
+
+    public void setMaxTemp(double maxTemp) {
+        this.maxTemp = maxTemp;
+    }
+
+    private double avgTemp;
+    private double devTemp;
+    private double minTemp;
+    private double maxTemp;
 
     public TempSummaryStatistics(double avgTemp, double devTemp, double minTemp, double maxTemp) {
         this.avgTemp = avgTemp;
